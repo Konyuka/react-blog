9996686d138413439901739f773b9f7638f13980
Michael Saiba <46402493+Konyuka@users.noreply.github.com>
2021-12-04 10:48:09 +0300

    getting static props

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import Layout from '../components/layout'
 import { getSortedPostsData } from '../lib/posts'
+import Image from 'next/image'
+import Head from 'next/head'
 
 
 export async function getStaticProps() {
@@ -16,7 +18,58 @@ export default function Home({ allPostsData }) {
   return (
 
     <Layout>
-      <div className="bg-gray-50 pt-12 sm:pt-16">
+      <Head>
+        <title>Landing Page</title>
+      </Head>
+
+      <div className="bg-white px-4 py-5 border-b border-gray-200 sm:px-6">
+        <div className="-ml-4 -mt-4 flex justify-between items-center flex-wrap sm:flex-nowrap">
+          <div className="ml-4 mt-4">
+            <div className="flex items-center">
+              <div className="flex-shrink-0">
+                <Image
+                  height={144}
+                  width={144}
+                 className="h-12 w-12 rounded-full" 
+                 src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" 
+                 alt="" />
+              </div>
+              <div className="ml-4">
+                <h3 className="text-lg leading-6 font-medium text-gray-900">
+                  Tom Cook
+                </h3>
+                <p className="text-sm text-gray-500">
+                  <a href="#">
+                    @tom_cook
+                  </a>
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="ml-4 mt-4 flex-shrink-0 flex">
+            <button type="button" className="relative inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+              <svg className="-ml-1 mr-2 h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
+              </svg>
+              <span>
+                Phone
+              </span>
+            </button>
+            <button type="button" className="ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+              <svg className="-ml-1 mr-2 h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
+                <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
+              </svg>
+              <span>
+                Email
+              </span>
+            </button>
+          </div>
+        </div>
+      </div>
+
+
+      <div className="bg-gray-50 sm:pt-2">
 
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="max-w-4xl mx-auto text-center">
@@ -26,39 +79,35 @@ export default function Home({ allPostsData }) {
           </div>
         </div>
 
-        <div className="mt-10 pb-12 bg-white sm:pb-16">
-          <div className="relative">
-            <div className="absolute inset-0 h-1/2 bg-gray-50"></div>
-            <div className="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-              <div className="max-w-4xl mx-auto">
-                <dl className="rounded-lg bg-white shadow-lg sm:grid sm:grid-cols-3">
+        <div class="grid grid-cols-1 gap-4 sm:grid-cols-2 p-5">
 
-                  {
-                    allPostsData.map(
-                      ({id, date, title}) => (
-                        <div key={id} className="flex flex-col border-b border-gray-100 p-6 text-center sm:border-0 sm:border-r">
-                          <dt className="order-2 mt-2 text-lg leading-6 font-medium text-gray-500">
-                            {id}
-                          </dt>
-                          <dt className="order-2 mt-2 text-lg leading-6 font-medium text-gray-500">
-                            {date}
-                          </dt>
-                          <dd className="order-1 text-lg font-bold text-indigo-600">
-                            {title}
-                          </dd>
-                        </div>
-                      )
-                    )
-                  }
+          {
+            allPostsData.map(({ id, date, title})=>(
 
-                  
-
-                </dl>
+              <div key={id} class="relative rounded-lg border border-gray-300 bg-white px-6 py-5 shadow-sm flex items-center space-x-3 hover:border-gray-400 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500">
+                <div class="flex-1 min-w-0">
+                  <a href="#" class="focus:outline-none">
+                    <span class="absolute inset-0" aria-hidden="true"></span>
+                    <p class="text-sm font-medium text-gray-900">
+                      {date}
+                    </p>
+                    <p class="text-sm text-gray-500 truncate">
+                      {title}
+                    </p>
+                  </a>
+                </div>
               </div>
-            </div>
-          </div>
+            )
+
+            )
+          }
+
+
         </div>
+
       </div>
     </Layout>
   )
 }
+
+
