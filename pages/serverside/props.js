8e5c1d41383095bfe208f8d0469aa1f822c2464f
Michael Saiba <46402493+Konyuka@users.noreply.github.com>
2021-12-04 13:42:50 +0300

    next image without specifying height and width

--- pages/serverside/props.js
+++ pages/serverside/props.js
@@ -48,15 +48,18 @@ export default function Home({ data }) {
                             <li className="relative">
                                 <div className="group block w-full aspect-w-10 aspect-h-7 rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-offset-gray-100 focus-within:ring-indigo-500 overflow-hidden">
                                     <Image
-                                        height={144}
-                                        width={144}
-                                        src="https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80" alt="" className="object-cover pointer-events-none group-hover:opacity-75" />
+                                        // height={600}
+                                        // width={500}
+                                        src="https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80" alt="" 
+                                        className="object-cover pointer-events-none group-hover:opacity-75" 
+                                         layout="fill"
+                                        />
                                     <button type="button" className="absolute inset-0 focus:outline-none">
                                 <span className="sr-only">{data.title}</span>
                                     </button>
                                 </div>
                                 <p className="mt-2 block text-sm font-medium text-gray-900 truncate pointer-events-none">{data.title}</p>
-                                <p className="block text-sm font-medium text-gray-500 pointer-events-none">3.9 MB</p>
+                                <p className="block text-sm font-medium text-gray-500 pointer-events-none">{data.overview}</p>
                             </li>
 
                         </ul>
